602c4f233352f30d9340f8e6083bced6bc67ddfc
Mariam Andreea Al-Farra <mariam_alfarra@hotmail.com>
2024-12-02 19:41:00 +0200

    updated

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
+    app: {
+        baseURL: "/AppDemo/"
+    },
     ssr:false,
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
