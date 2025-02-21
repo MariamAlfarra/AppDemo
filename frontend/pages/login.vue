a08f3b19ead685a8f6dc33390a4ef43811ea02e1
Mariam Andreea Al-Farra <mariam_alfarra@hotmail.com>
2025-02-21 11:10:27 +0200

    updated login.ts

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -73,7 +73,8 @@ async function onSubmit() {
                             <UInput v-model="state.password"
                                     :type="isPasswordVisible ? 'text' : 'password'"
                                     placeholder="Password"
-                                    class="w-full rounded shadow-inner" />
+                                    class="w-full rounded shadow-inner" 
+                                    :input-attrs="{autocomplete: 'current-password'}" />
                             <button type="button" class="absolute right-3 top-2 text-gray-500 hover:text-gray-700 focus:outline-none" @click="togglePasswordVisibility">
                                 <UIcon :name="isPasswordVisible ? 'i-heroicons-eye' : 'i-heroicons-eye-slash'" class="text-gray-200" />
                             </button>
